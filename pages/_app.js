e1698ac8d51644b06bc56462bc3a6e95d248d008
wahidari <wahiid.ari@gmail.com>
2022-12-05 17:02:15 +0700

    using next-themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-import { GlobalProvider } from "@utils/GlobalContext";
+// import { GlobalProvider } from "@utils/GlobalContext";
 import { AxiosConfigProvider } from "@utils/useAxiosConfig";
 import "@styles/globals.css";
+import { Toaster } from 'react-hot-toast';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalProvider>
-      <AxiosConfigProvider>
-        <Component {...pageProps} />
-      </AxiosConfigProvider>
-    </GlobalProvider>
+    <ThemeProvider attribute="class" storageKey='theme'>
+      {/* <GlobalProvider> */}
+        <AxiosConfigProvider>
+          <Component {...pageProps} />
+          <Toaster />
+        </AxiosConfigProvider>
+      {/* </GlobalProvider> */}
+    </ThemeProvider>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='id'>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
